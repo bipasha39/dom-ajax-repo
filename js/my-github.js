e94f1fde71f714c3a5354f33e79e75e495e60e22
bipasha39 <physiobipasha39@gmail.com>
2020-10-05 13:41:38 +0200

    fatch and debugging

--- js/my-github.js
+++ js/my-github.js
@@ -3,7 +3,7 @@ function fetchGithubEndpoint(endpoint) {
   const githubAPIBaseURL = "https://api.github.com";
   fetch(`${githubAPIBaseURL}${endpoint}`)
     .then(function(response) {
-      console.log(response.status);
+      console.log(response);
       return response.json();
     })
     .then(function(repositories) {
@@ -13,10 +13,16 @@ function fetchGithubEndpoint(endpoint) {
       for (let i = 0; i < repositories.length; i++) {
         const repository = repositories[i];
         const repositoryElement = document.createElement("li");
-        repositoryElement.innerHTML = repository.name;
+        let a = document.createElement("a");
+        a.setAttribute("href", repository.html_url);
+        a.innerHTML = repository.name;
+        repositoryElement.appendChild(a);
         reposList.appendChild(repositoryElement);
+        let x = document.getElementById("repos-count");
+        x.innerHTML = repositories.length;
       }
     });
 }
-const userName = "bipasha39";
-fetchGithubEndpoint(`/users/${userName}/repos`);
+
+const userName = "ayamaui";
+fetchGithubEndpoint(`/repositories`);
